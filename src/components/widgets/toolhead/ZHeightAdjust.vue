42218e9b0f80f01fcd0709b655ac7d6e078a4659
Sergei Rozhkov <fgup@zserg.ru>
2025-12-23 12:46:36 +0500

    FROM=fluidd

--- src/components/widgets/toolhead/ZHeightAdjust.vue
+++ src/components/widgets/toolhead/ZHeightAdjust.vue
@@ -182,7 +182,7 @@ export default class ZHeightAdjust extends Mixins(StateMixin) {
    */
   sendZAdjustGcode (direction: '+' | '-') {
     const zHomed: boolean = this.$typedGetters['printer/getHomedAxes']('z')
-    const gcode = `SET_GCODE_OFFSET Z_ADJUST=${direction}${this.moveDistance} MOVE=${+zHomed}`
+    const gcode = `SET_GCODE_OFFSET Z_ADJUST=${direction}${this.moveDistance} MOVE=${+zHomed} FROM=fluidd`
     this.sendGcode(gcode, this.$waits.onZAdjust)
   }
 
